39ff32066c6e0b2767929566f3aea151ddcbfecf
Ervis Trupja <ervistrupja@outlook.com>
2021-11-11 11:46:59 +0100

    Initial commit

---
+++ scripts/site.js
@@ -0,0 +1,6 @@
+function addAuthor(){
+    let fName = document.getElementById('fname').value;
+    let lName = document.getElementById('lname').value;
+
+    console.log('Full name: '+fName+' '+lName);
+}
