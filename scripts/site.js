263377fb10643217516e4f33dbd0f94ae25dc523
Ervis Trupja <ervistrupja@outlook.com>
2021-11-17 22:48:09 +0100

    Revert "Completed all forms"
    This reverts commit 54009549009384fa43492f52323d8639655e2fb6.

--- scripts/site.js
+++ scripts/site.js
@@ -4,12 +4,3 @@ function addAuthor(){
 
     console.log('Full name: '+fName+' '+lName);
 }
-
-function addBook(){
-    let title = document.getElementById('title').value;
-    let description = document.getElementById('description').value;
-    let authorId = document.getElementById('authorId').value;
-    let rate = document.getElementById('rate').value;
-
-    console.log('Book details: '+title+' '+description+' '+authorId+' '+rate);
-}
