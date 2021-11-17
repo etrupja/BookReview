54009549009384fa43492f52323d8639655e2fb6
Ervis Trupja <ervistrupja@outlook.com>
2021-11-17 21:52:31 +0100

    Completed all forms

--- scripts/site.js
+++ scripts/site.js
@@ -4,3 +4,12 @@ function addAuthor(){
 
     console.log('Full name: '+fName+' '+lName);
 }
+
+function addBook(){
+    let title = document.getElementById('title').value;
+    let description = document.getElementById('description').value;
+    let authorId = document.getElementById('authorId').value;
+    let rate = document.getElementById('rate').value;
+
+    console.log('Book details: '+title+' '+description+' '+authorId+' '+rate);
+}
