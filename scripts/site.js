eb90bb4786f7e92bc050d301ca6afaf004811e00
etrupja <ervistrupja@outlook.com>
2021-11-18 11:39:24 +0100

    validation added for addAuthor
    asdasdas

--- scripts/site.js
+++ scripts/site.js
@@ -1,9 +1,33 @@
-function addAuthor(){
-    let fName = document.getElementById('fname').value;
-    let lName = document.getElementById('lname').value;
+$(document).ready(function(){
+    $("#addAuthorForm").validate({
+        rules:{
+            fname: { 
+                required: true, 
+                minlength: 3
+            },
+            lname: { required: true}
+        },
+        messages:{
+            fname: { 
+                required: 'First name is required', 
+                minlength: 'Min length required is 3 chars'
+            },
+            lname: 'Last name is required',
+        },
+        //messages
+        submitHandler: function(form){
+            console.log(`${form.fname.value} ${form.lname.value}`);
+        }
+    })
+});
 
-    console.log('Full name: '+fName+' '+lName);
-}
+
+// function addAuthor(){
+//     let fName = document.getElementById('fname').value;
+//     let lName = document.getElementById('lname').value;
+
+//     console.log('Full name: '+fName+' '+lName);
+// }
 
 function addBook(){
     let title = document.getElementById('title').value;
@@ -11,5 +35,5 @@ function addBook(){
     let authorId = document.getElementById('authorId').value;
     let rate = document.getElementById('rate').value;
 
-    console.log(title+' '+description+' '+authorId+' '+rate);
+    console.log(`${title} ${description} ${authorId} ${rate}`);
 }
